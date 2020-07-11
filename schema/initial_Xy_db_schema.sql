bce3b325e43d1d077302837a18cae0ff77fa9ef3
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-07-11 18:36:04 -0400

    adding schema file

---
+++ schema/initial_Xy_db_schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE review(
+	review_id VARCHAR NOT NULL,
+	review_text TEXT NOT NULL,
+	stars INT NOT NULL,
+	cool INT NOT NULL,
+	useful INT NOT NULL,
+	funny INT NOT NULL,
+	review_date date NOT NULL, 
+	review_type varchar NOT NULL,
+	PRIMARY KEY (review_id)
+);
+
+CREATE TABLE business(
+	review_id VARCHAR NOT NULL,
+	business_id VARCHAR NOT NULL,
+	FOREIGN KEY (review_id) REFERENCES review (review_id),
+	PRIMARY KEY (review_id, business_id)
+);
+
+CREATE TABLE yelp_user(
+	review_id VARCHAR NOT NULL,
+	user_id VARCHAR NOT NULL,
+	FOREIGN KEY (review_id) REFERENCES review (review_id),
+	PRIMARY KEY (review_id)
+);
